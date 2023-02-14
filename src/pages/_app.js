1e1b37bcb61d7205c0de54c188a7e884f5fa2c51
Vedant Chainani <vedantchainani1084@gmail.com>
2023-02-14 14:20:52 +0530

    Add Tailwind and RainbowKit Integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,73 @@
-import '@/styles/globals.css'
+// CSS Imports
+import "@/styles/globals.css";
+
+// RainbowKit Imports
+import {
+  connectorsForWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import {
+  metaMaskWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  braveWallet,
+  coinbaseWallet,
+  trustWallet,
+  argentWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import "@rainbow-me/rainbowkit/styles.css";
+
+// Wagmi Imports
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID })]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      metaMaskWallet({ chains }),
+      walletConnectWallet({ chains }),
+      coinbaseWallet({ chains }),
+      trustWallet({ chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [
+      braveWallet({ chains }),
+      rainbowWallet({ chains }),
+      argentWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        modalSize="wide"
+        chains={chains}
+        coolMode
+        appInfo={{
+          appName: "Vedant Chainani Blog",
+          learnMoreUrl: "https://envoyvc.dev",
+        }}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
